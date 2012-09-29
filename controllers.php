1b730cda964bd3f2eea0973e01824a3ba1d18206
Alwin Karabiowski <alwin.karabiowski@adcloud.com>
2012-09-29 14:08:45 +0200

    deleted files from wrong project

--- controllers.php
+++
@@ -1,7 +0,0 @@
-<?php
-$application = Application::getInstance();
-$application->addController(new RegisterController(), "register");
-$application->addController(new LoginController(), "login");
-$application->addController(new CreateLightningUploadController(), "createLightningUpload");
-$application->addController(new CreateLightningTextfieldController(), "createLightningTextarea");
-?>
